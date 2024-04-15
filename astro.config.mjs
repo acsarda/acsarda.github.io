a6f29a09c17e3282bcc7d168db8260cc8474cbcd
Augusto <sardaugusto@gmail.com>
2024-04-15 11:53:20 +0100

    commit trying to deploy...

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://acsarda.github.io',
-  // base: 'acsarda.github.io',
+    site: 'https://acsarda.github.io'
 });
