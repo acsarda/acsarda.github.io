c668cc5c12fedaaa1680420b9efae7acfbf3f042
Augusto <sardaugusto@gmail.com>
2024-04-15 11:45:15 +0100

    astro config file for deploy web

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://acsarda.github.io',
+  // base: 'acsarda.github.io',
+});
